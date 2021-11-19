ae495d87a0105d6aff02f687e0e18181b435f3d6
Tiago de Carvalho <tiago.de.carvalho@hotmail.com>
2021-11-19 19:54:38 -0300

    Testando Date() para api

--- wwwroot/js/fusos.js
+++ wwwroot/js/fusos.js
@@ -5,6 +5,8 @@
  *************/
 
  $(document).ready(function () {
+
+    var datetime = new Date("2021-11-18T10:21:49.451139-03:00");
     
     var options = {
         method: 'GET',
@@ -12,12 +14,41 @@
         cache: 'default'
     }
 
-    fetch("https://worldtimeapi.org/api/ip", options)
+    console.log(`${datetime.getFullYear()} - ${datetime.getMonth()} - ${datetime.getDate()}`)
+    console.log(`${datetime.getHours()} - ${datetime.getMinutes()} - ${datetime.getSeconds()}`)
+    console.log(datetime.getTime())
+
+    //milesimosParaRelogio(1637155899);
+
+    /*fetch("http://worldtimeapi.org/api/ip", options)
     .then( response => { response.json()
-        .then( data => 
-           $("#horaCerta").html(data.datetime)
-        )
+        .then( data => { mostraHora(data) })
     })
-    .catch(e => console.log("Errou!: " + e.message))
+    .catch(e => console.log("Errou!: " + e.message))*/
+
+    function mostraHora(dateJson){
+        console.log(moment(dateJson.datetime).format("H:mm:ss"))
+        $("#horaCerta").html(moment(dateJson.datetime).format("H:mm:ss"))
+        //milesimosParaRelogio(dateJson.unixtime);
+    }
+
+    function milesimosParaRelogio(milesimos){
+
+        var milsec  = 0;
+        var ss      = 0;
+        var mm      = 0;
+        var hh      = 0;
+
+        ss      = Math.floor(milesimos / 1000) //transforma de milesimos para segundos
+        milsec  = milesimos % 1000             //nao deixa passar de 999 milesimos
+        mm      = Math.floor(ss / 60)          //transforma de segundos para minutos
+        ss      = ss % 60                      //nao deixa passar de 59 segundos
+        hh      = Math.floor(mm / 60)          //transforma minutos em horas
+        mm      = mm % 60
+        
+        var formatoCronometro = ( hh < 10 ? "0" + hh : hh ) + ":" + ( mm < 10 ? "0" + mm : mm ) + ":" + ( ss < 10 ? "0" + ss : ss ) + "."
+        //$("#testeMilessimosHora").html(formatoCronometro)
+        $("#testeMilessimosHora").html(moment("1637155899", "YYYYMMDD").fromNow());
+    }
 
 });
