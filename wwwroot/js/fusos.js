0f1a3b826669277e0719dab50be6aaa7444afed4
Tiago de Carvalho <tiago.de.carvalho@hotmail.com>
2021-12-13 22:32:13 -0300

    v1.0 - sistema no ar

--- wwwroot/js/fusos.js
+++ wwwroot/js/fusos.js
@@ -6,28 +6,41 @@
 
 // to do: acesso regular a api para atualização constante da hora
 
- $(document).ready(function () {s
+ $(document).ready(function () {
 
-    var relogioFuso
-    //var datetime           = new Date()
+    var relogioFuso        = null
     var tempoInicioRelogio = new Date()
     var fusoAPI            = new Date()
 
-    moment.locale('pt-br')
-    
     var options = {
         method: 'GET',
         mode: 'cors',
         cache: 'default'
     }
 
-    fetch("http://worldtimeapi.org/api/ip", options)
-    .then( response => { response.json()
-        .then( data => { mostraHora(data) })
-    })
-    .catch(e => console.log("Errou!: " + e.message))
+    moment.locale('pt-br')
+    chamadaAPI()
+    setInterval(chamadaAPI, 1000 * 61)
+
+    /*
+    * Funções  
+    */
+
+    function chamadaAPI(){
+
+        if(relogioFuso != null){
+            clearTimeout(relogioFuso)
+        }
+
+        fetch("http://worldtimeapi.org/api/ip", options)
+        .then( response => { response.json()
+            .then( data => { mostraHora(data) })
+        })
+        .catch(e => console.log("Errou!: " + e.message))
+    }
 
     function mostraHora(dateJson){
+
         atualizaInicioRelogio()
         $("#horaCerta").html(moment(dateJson.datetime).format("H:mm:ss"))
         $("#data").html(moment(dateJson.datetime).format('dddd') + ', ' + moment(dateJson.datetime).format('LL'))
@@ -48,11 +61,7 @@
 
         $("#horaCerta").html(moment(unixHoraAPI).format("H:mm:ss"))
 
-        /*console.log(moment(unixHoraAPIcopy).format("HH:mm:ss"))
-        console.log(tempoPercorrido)*/
-
         relogioFuso = setTimeout(rodaRelogio, 300);
-
     }
 
     function atualizaInicioRelogio(){
